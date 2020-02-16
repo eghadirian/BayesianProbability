2c58526d5e97df7a8748e9e977f1ea913139bb46
eghadirian <60559670+eghadirian@users.noreply.github.com>
2020-02-16 17:45:47 -0500

    encapsulation

--- P8-PyMC3(MultiVariables-5D).py
+++ P8-PyMC3(MultiVariables-5D).py
@@ -13,28 +13,34 @@ y_2 = (1. + rand)*(x_1 * x_2) # alpha_2 * x_1 * x_2
 y_3 = np.sin(x_2 ** (1. + rand)) # alpha_3 * sin(x_2 ^ gamma_1)
 
 # Regression functions
-def fun_lin(x_1, alpha_1, beta_1): 
-    return alpha_1 * x_1 + beta_1
-def fun_nlin_bi(x_1, x_2, alpha_2): 
-    return alpha_2 * x_1 * x_2
-def fun_nlin_un(x_2, alpha_3, gamma_1): 
-    return alpha_3 * np.sin(x_2 ** gamma_1)
+def encapsulate(x_1, x_2, alpha_1, alpha_2, alpha_3, beta_1, gamma_1):
+    def fun_lin(x_1, alpha_1, beta_1): 
+      return alpha_1 * x_1 + beta_1
+    def fun_nlin_bi(x_1, x_2, alpha_2): 
+        return alpha_2 * x_1 * x_2
+    def fun_nlin_un(x_2, alpha_3, gamma_1): 
+        return alpha_3 * np.sin(x_2 ** gamma_1)
+    return (fun_lin(x_1, alpha_1, beta_1), \
+            fun_nlin_bi(x_1, x_2, alpha_2), \
+            fun_nlin_un(x_2, alpha_3, gamma_1))
 
 # Bayesian inference
 if __name__=='__main__':
-    with pm.Model() as model_1:
+    with pm.Model() as model:
         alpha_1 = pm.Normal('alpha1', mu=0., sigma=1., testval=0.)
         alpha_2 = pm.Normal('alpha2', mu=0., sigma=1., testval=0.)
         alpha_3 = pm.Normal('alpha3', mu=0., sigma=1., testval=0.)
         beta_1 = pm.Normal('beta1', mu=0., sigma=1., testval=0.)
         gamma_1 = pm.Gamma('gamma1', alpha=5., beta=5.)
-        y1 = pm.Deterministic('y1', fun_lin(x_1, alpha_1, beta_1))
-        y2 = pm.Deterministic('y2', fun_nlin_bi(x_1, x_2, alpha_2))
-        y3 = pm.Deterministic('y3', fun_nlin_un(x_2, alpha_3, gamma_1))
+        y1_det, y2_det, y3_det = \
+        encapsulate(x_1, x_2, alpha_1, alpha_2, alpha_3, beta_1, gamma_1)
+        y1 = pm.Deterministic('y1', y1_det)
+        y2 = pm.Deterministic('y2', y2_det)
+        y3 = pm.Deterministic('y3', y3_det)
         observed = pm.Normal('obs', tt.stack([y1, y2, y3]),\
             observed=tt.stack([y_1, y_2, y_3]))
-        trace_1 = pm.sample(120000, step=pm.NUTS(), start=pm.find_MAP())
-        burned_trace = trace_1[100000::2]
+        trace = pm.sample(120000, step=pm.NUTS(), start=pm.find_MAP())
+        burned_trace = trace[100000::2]
     alpha1_samples = burned_trace["alpha1"][:, None]
     alpha2_samples = burned_trace["alpha2"][:, None]
     alpha3_samples = burned_trace["alpha3"][:, None]
