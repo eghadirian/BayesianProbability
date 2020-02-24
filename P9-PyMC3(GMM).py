7b8f58237774f44e3976aba6f961bc1deb4e4043
eghadirian <60559670+eghadirian@users.noreply.github.com>
2020-02-23 21:49:33 -0500

    posterior plots

--- P9-PyMC3(GMM).py
+++ P9-PyMC3(GMM).py
@@ -3,6 +3,7 @@ import numpy as np
 import matplotlib.pyplot as plt 
 import pymc3 as pm 
 
+# mock data
 np.random.seed(42)
 n_data = 1000
 weight = np.array([0.2, 0.3, 0.5])
@@ -11,15 +12,28 @@ sigma = np.array([.75, .5, 1.])
 comp = np.random.choice(mu.size, size=n_data, p=weight)
 x_data = np.random.normal(mu[comp], sigma[comp], size=n_data)
 plt.figure()
-plt.hist(x_data, bins=200)
+plt.hist(x_data, bins=200, label=r'Actual Data')
 
+# inference
 if __name__=='__main__':
     with pm.Model() as model:
         w=pm.Dirichlet('w', np.ones_like(weight))
         mu=pm.Normal('mu', 0., 10., shape=weight.size)
         tau=pm.Gamma('tau', 1., 1., shape=weight.size)
-        x_observed = pm.NormalMixture('x_observed', w, mu, tau=tau, observed=x_data)
+        x_observed = pm.NormalMixture('x_observed', w, mu, tau=tau, \
+                                      observed=x_data)
         trace=pm.sample(5000, n_init=10000, tune=1000, random_seed=42)
     plt.figure()
-    plt.hist(trace['mu'], bins=50)
+    plt.hist(trace['w'], bins=50, label=r"posterior of $\weights$")
+    plt.figure()
+    plt.hist(trace['mu'], bins=50, label=r"posterior of $\mus$")
+    plt.figure()
+    plt.hist(trace['tau'], bins=50, label=r"posterior of $\taus$")
+    with model:
+        p_trace = pm.sample_posterior_predictive(trace, 5000, random_seed=42)
+    plt.figure()
+    plt.hist(p_trace['x_observed'], bins=50, density=True, histtype='step',\
+        lw=2, alpha=.05, label='Posterior predictive distribution')
+    plt.hist(x_data, bins=50, density=True, histtype='step', lw=2, \
+        label='Observed data')
 plt.show()
