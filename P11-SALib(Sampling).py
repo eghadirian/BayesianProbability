3561d6f6e053b70158b9a864ebc8b5ba95e88d9b
eghadirian <60559670+eghadirian@users.noreply.github.com>
2020-02-25 20:27:33 -0500

    adding nonlinearity

--- P11-SALib(Sampling).py
+++ P11-SALib(Sampling).py
@@ -9,7 +9,7 @@ param_values = saltelli.sample(problem, 1000)
 z = np.zeros([param_values.shape[0]])
 
 def evaluate_model(P):
-    return 2.*P[0]+P[1]**2
+    return 2.*P[0]+P[1]**2+np.exp(P[0]*P[1])
 
 for i, P in enumerate(param_values):
     z[i] = evaluate_model(P)
