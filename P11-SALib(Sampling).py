4c54194f42051ea065b2a9d87b45cadecef13ed8
eghadirian <60559670+eghadirian@users.noreply.github.com>
2020-02-25 20:26:29 -0500

    adding sensitivity analysis

---
+++ P11-SALib(Sampling).py
@@ -0,0 +1,20 @@
+from SALib.sample import saltelli
+from SALib.analyze import sobol
+import numpy as np 
+
+problem={'num_vars':2,\
+    'names':['x','y'],\
+        'bounds':[[-1,1], [0,2]]}
+param_values = saltelli.sample(problem, 1000)
+z = np.zeros([param_values.shape[0]])
+
+def evaluate_model(P):
+    return 2.*P[0]+P[1]**2
+
+for i, P in enumerate(param_values):
+    z[i] = evaluate_model(P)
+
+Si = sobol.analyze(problem, z)
+print('First-Order Sensitivity:{}\nTotal-Order Sensitivity{}'\
+    .format(Si['S1'],Si['ST']))
+
