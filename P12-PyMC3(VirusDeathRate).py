24a21bd4d143a6316a338d86055afe61fcff6563
eghadirian <60559670+eghadirian@users.noreply.github.com>
2020-03-06 00:47:51 -0500

    Got the data on 03/05/20 (WHO), sigma~1%, mu=3.5%

---
+++ P12-PyMC3(VirusDeathRate).py
@@ -0,0 +1,33 @@
+import pandas as pd 
+import matplotlib.pyplot as plt 
+import pymc3 as pm 
+from google.colab import files
+
+uploaded = files.upload()
+df = pd.read_excel('Book1.xlsx').fillna(0)
+number = df['Total Cases']
+percent = df['Total Deaths']/df['Total Cases']
+
+occurances = []
+for i in range(len(number)):
+    for j in range(number[i]):
+        occurances.append(percent[i])
+
+if __name__=='__main__':
+    with pm.Model() as modl:
+        p = pm.Normal('Death Rate', mu=0.02, sigma=0.1)
+        sigma = pm.Normal('sigma', mu=0.01, sigma=0.1)
+        obs = pm.Normal('observation', mu=p, sigma=sigma, observed=occurances)
+        trace = pm.sample(200000, step=pm.Metropolis())
+        burned_trace = trace[150000::2]
+    mu_samples = burned_trace['Death Rate'][:, None]
+    s_samples = burned_trace['sigma'][:, None]
+    plt.figure()
+    plt.hist(mu_samples, bins=30, label=r"posterior of $\Death Rate$", \
+             color="#A60628", normed=True)
+    plt.figure()
+    plt.hist(s_samples, bins=30, label=r"posterior of $\Standard Deviation$", \
+             color="#A60628", normed=True)
+
+
+
